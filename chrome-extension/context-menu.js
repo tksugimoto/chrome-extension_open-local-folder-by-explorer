b3bfe7a82b8e381069bee251e5a58c399ecb8166
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-05-13 00:53:09 +0900

    feat: ContextMenu title を storage から取得するように変更
    キー
    - contextMenus.title.PAGE
    - contextMenus.title.LINK
    - contextMenus.title.SELECTION
    
    ※ 「"minimum_chrome_version": "73"」 は Object.fromEntries 使用のため
    ※ storage に保存する機能は未実装

--- chrome-extension/context-menu.js
+++ chrome-extension/context-menu.js
@@ -1,40 +1,62 @@
 export const CONTEXT_MENU = {
 	PAGE: {
 		id: 'page',
-		title: 'フォルダをExplorerで開く',
+		defaultTitle: 'フォルダをExplorerで開く',
 	},
 	LINK: {
 		id: 'link',
-		title: 'リンク先をExplorerで開く（ローカルファイルの場合）',
+		defaultTitle: 'リンク先をExplorerで開く（ローカルファイルの場合）',
 	},
 	SELECTION: {
 		id: 'selection',
-		title: '選択文字列をExplorerで開く（ローカルファイルパスの場合）',
+		defaultTitle: '選択文字列をExplorerで開く（ローカルファイルパスの場合）',
 	},
 };
 
+const generateStorageKey = key => `contextMenus.title.${key}`;
+
+const getContextMenuTitle = () => {
+	const keys = Object.keys(CONTEXT_MENU).map(generateStorageKey);
+	return new Promise(resolve => {
+		chrome.storage.local.get(keys, items => {
+			const contextMenuTitlePair = Object.entries(CONTEXT_MENU).map(([key, { defaultTitle }]) => {
+				const storageKey = generateStorageKey(key);
+				const title = items[storageKey] || defaultTitle;
+				return [
+					key,
+					title,
+				];
+			});
+			const contextMenuTitle = Object.fromEntries(contextMenuTitlePair);
+			resolve(contextMenuTitle);
+		});
+	});
+};
+
 export const createContextMenu = () => {
-	chrome.contextMenus.create({
-		title: CONTEXT_MENU.PAGE.title,
-		contexts: ['page'],
-		documentUrlPatterns: [
-			'file:///*',
-		],
-		id: CONTEXT_MENU.PAGE.id,
-	});
-	chrome.contextMenus.create({
-		title: CONTEXT_MENU.LINK.title,
-		contexts: ['link'],
-		targetUrlPatterns: [
-			// file:///* 指定だとローカルファイルリンクにメニューが表示されないため <all_urls> 指定している
-			// ※ targetUrlPatterns 指定を無しにしてもOk
-			'<all_urls>',
-		],
-		id: CONTEXT_MENU.LINK.id,
-	});
-	chrome.contextMenus.create({
-		title: CONTEXT_MENU.SELECTION.title,
-		contexts: ['selection'],
-		id: CONTEXT_MENU.SELECTION.id,
+	getContextMenuTitle().then(contextMenuTitle => {
+		chrome.contextMenus.create({
+			title: contextMenuTitle.PAGE,
+			contexts: ['page'],
+			documentUrlPatterns: [
+				'file:///*',
+			],
+			id: CONTEXT_MENU.PAGE.id,
+		});
+		chrome.contextMenus.create({
+			title: contextMenuTitle.LINK,
+			contexts: ['link'],
+			targetUrlPatterns: [
+				// file:///* 指定だとローカルファイルリンクにメニューが表示されないため <all_urls> 指定している
+				// ※ targetUrlPatterns 指定を無しにしてもOk
+				'<all_urls>',
+			],
+			id: CONTEXT_MENU.LINK.id,
+		});
+		chrome.contextMenus.create({
+			title: contextMenuTitle.SELECTION,
+			contexts: ['selection'],
+			id: CONTEXT_MENU.SELECTION.id,
+		});
 	});
 };
