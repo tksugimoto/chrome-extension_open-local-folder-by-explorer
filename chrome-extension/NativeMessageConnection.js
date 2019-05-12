ce64e3ffb06e58e730df1ea76f725b16a5c8f238
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-05-12 22:12:26 +0900

    fix: コネクション切断で復旧できない問題を修正
    postMessage(message) 時に切断されている場合、再接続する処理を追加

--- chrome-extension/NativeMessageConnection.js
+++ chrome-extension/NativeMessageConnection.js
@@ -3,12 +3,14 @@ class NativeMessageConnection {
     constructor(applicationName) {
         this._applicationName = applicationName;
         this._callbacks = [];
+        this.isActive = false;
         this._connect();
     }
 
     _connect() {
         this._port = chrome.runtime.connectNative(this._applicationName);
         this._setupListener();
+        this.isActive = true;
     }
 
     _setupListener() {
@@ -17,10 +19,16 @@ class NativeMessageConnection {
             const callback = this._callbacks.shift();
             callback(message);
         });
+        this._port.onDisconnect.addListener(() => {
+            this.isActive = false;
+        });
     }
 
     postMessage(message) {
         return new Promise(resolve => {
+            if (!this.isActive) {
+                this._connect();
+            }
             this._callbacks.push(resolve);
             this._port.postMessage(message);
         });
