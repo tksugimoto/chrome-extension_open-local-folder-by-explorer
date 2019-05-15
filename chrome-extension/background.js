b629a59b411e73ca02f0d16ee84b71b235c62d60
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2019-05-16 00:42:39 +0900

    fix: path に # が含まれる場合にエクスプローラーで開けない問題を修正
    URLでは # は %23 で表されるがdecodeURI では %23 を # に変換しないため
    decode 処理を decodeURIComponent に変更
    
    ※ 本来 decodeURIComponent はURL全体に対して使うものではないが、file path の場合は問題ないので使用する

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -107,7 +107,7 @@ const extractFilePath = info => {
 };
 
 const convertUrl2FilePath = encodedUrl => {
-	const decodedURI = decodeURI(encodedUrl);
+	const decodedURI = decodeURIComponent(encodedUrl);
 	if (decodedURI.startsWith('file:///')) {
 		return decodedURI.replace(/^file:\/\/\//, '').replace(/\//g, '\\');
 	}
